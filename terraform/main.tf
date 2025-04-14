1612b640cee8adde9db33a27343749ed2f4aa167
Agustincorreag91 <129911123+Agustincorreag91@users.noreply.github.com>
2025-04-14 01:04:46 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,15 +20,15 @@ module "ec2_instance" {
 module "eks_cluster" {
   source = "./modules/eks"
   
-  cluster_name     = var.cluster_name
-  cluster_version  = var.cluster_version
-  vpc_id           = var.vpc_id
-  subnet_ids       = var.subnet_ids
-  node_group_name  = var.node_group_name
-  instance_types   = var.node_instance_types
-  desired_capacity = var.desired_capacity
-  min_size         = var.min_size
-  max_size         = var.max_size
+#  cluster_name     = var.cluster_name
+# cluster_version  = var.cluster_version
+#vpc_id           = var.vpc_id
+#  subnet_ids       = var.subnet_ids
+#  node_group_name  = var.node_group_name
+#  instance_types   = var.node_instance_types
+#  desired_capacity = var.desired_capacity
+#  min_size         = var.min_size
+#  max_size         = var.max_size
   
   depends_on = [module.ec2_instance]
 }
