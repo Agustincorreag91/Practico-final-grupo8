aa8ece1e28fb93c7eb3c39e8a63b42b869ff46b3
Agustincorreag91 <129911123+Agustincorreag91@users.noreply.github.com>
2025-04-14 00:57:04 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -36,6 +36,7 @@ variable "subnet_id" {
 variable "security_groups" {
   description = "Lista de grupos de seguridad para la instancia"
   type        = list(string)
+  default     = "Pin2"
 }
 
 variable "tags" {
